0fc3806741f02d7b0e0f560377694e074c8df25d
Ayush Yadav <ayushhyadav.003@gmail.com>
2026-09-16 20:04:16 +0530

    Safe catch in api/index.js to prevent FUNCTION_INVOCATION_FAILED

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ const app = require('../backend/server');
 const connectDB = require('../backend/config/db');
 
 module.exports = async (req, res) => {
-  await connectDB();
+  try {
+    await connectDB();
+  } catch (err) {
+    console.error('[Serverless DB Error]:', err.message);
+  }
   return app(req, res);
 };
